747e523126dc22df47c2aaf2b444d8771e13cb0d
kwamebrowne <theeecryptoking@gmail.com>
2023-05-26 23:07:00 -0500

    these read mes need an interperter

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,15 @@
 class Vehicle:
-    pass
+
+     # parent / superclass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    # everything below is a child class
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_tank(self):
+        return "filling up!"
+    
+
